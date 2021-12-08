59351f0cb5fb79c71f51bfdd89a0f8f766df205a
Susan Tan <zujunt@della-gpu.princeton.edu>
2021-12-07 20:51:20 -0500

    change preprocessing for obfuscation

--- preprocess.sh
+++ preprocess.sh
@@ -3,7 +3,7 @@
 python -m codegen_sources.preprocessing.preprocess \
   /home/zujunt/xcellent-ml/CodeGen/fake_data \
   --langs python \
-  --mode=monolingual \
+  --mode=obfuscation \
   --local=True \
   --bpe_mode=roberta \
   --train_splits=1 \
